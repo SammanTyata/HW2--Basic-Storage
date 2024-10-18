ca3837a7dfc6a61bcfe0a256dd886cb007aedfd5
Samman <tyatasamman@gmail.com>
2024-10-18 16:19:12 -0700

    Initial Commit

---
+++ HW2--Basic-Storage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW2--Basic-Storage
+//
+//  Created by Samman Tyata on 10/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW2--Basic-Storage/HW2__Basic_StorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW2__Basic_StorageApp.swift
+//  HW2--Basic-Storage
+//
+//  Created by Samman Tyata on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct HW2__Basic_StorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
